61af70e74f594a4625d67c57bc697abdc9ec3e42
OPC-16 <chavan.p.omkar@gmail.com>
2023-05-07 09:53:26 +0530

    Add maths.cpp (basic)
    this is just a intro file and we will cover maths in more depth

---
+++ 11. maths.cpp
@@ -0,0 +1,13 @@
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+/* maths in graphics programming mainly deals with 2 things - matrices and vectors 
+ * matrix -> array of numbers, and matrices can be useful for positioning objects in 3D world
+ * vector -> it has 2 types directional and positional vectors, it has direction and magnitude 
+ *
+ * maths is used in transformation, scaling and rotating*/
+
+/* we will use the glm library for doing the maths stuff in OpenGL
+ * https://github.com/g-truc/glm 
+ * it is a header only library i.e. there is no cpp file all is implemented in the .h files
+ * */
